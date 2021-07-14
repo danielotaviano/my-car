0076c371db8fe1019b04dc7a557f5a819b7a4a29
daniel otaviano <danielotavianopontes@gmail.com>
2021-07-14 20:20:48 -0300

    feat: ensure AddCarController reutrns 500 if AddAccount throws

--- src/presentation/controllers/car-controllers/add-car/add-car-controller.ts
+++ src/presentation/controllers/car-controllers/add-car/add-car-controller.ts
@@ -1,12 +1,17 @@
 import { AddCar } from '@/domain/usecases/car'
+import { serverError } from '@/presentation/helpers/http/http.helper'
 import { Controller } from '@/presentation/protocols/controller'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 
 export class AddCarController implements Controller {
   constructor(private readonly addCar: AddCar) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const carData = httpRequest.body
-    await this.addCar.add(carData)
-    return null
+    try {
+      const carData = httpRequest.body
+      await this.addCar.add(carData)
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
