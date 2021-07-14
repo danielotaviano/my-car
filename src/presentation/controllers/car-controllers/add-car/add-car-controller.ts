9aaf4a8650b58f202e469dc334c37e646899c5d1
daniel otaviano <danielotavianopontes@gmail.com>
2021-07-14 20:49:00 -0300

    feat: ensure that AddCarController call validation with correct values

--- src/presentation/controllers/car-controllers/add-car/add-car-controller.ts
+++ src/presentation/controllers/car-controllers/add-car/add-car-controller.ts
@@ -2,12 +2,18 @@ import { AddCar } from '@/domain/usecases/car'
 import { ok, serverError } from '@/presentation/helpers/http/http.helper'
 import { Controller } from '@/presentation/protocols/controller'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
+import { Validation } from '@/presentation/protocols/validation'
 
 export class AddCarController implements Controller {
-  constructor(private readonly addCar: AddCar) {}
+  constructor(
+    private readonly addCar: AddCar,
+    private readonly validation: Validation
+  ) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const carData = httpRequest.body
+      this.validation.validate(carData)
       const savedCar = await this.addCar.add(carData)
       return ok(savedCar)
     } catch (error) {
