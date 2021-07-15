d3388b1ee458323e2796c6897e9f59a1a27cc3c7
daniel otaviano <danielotavianopontes@gmail.com>
2021-07-15 00:47:03 -0300

    feat: ensure DbUpdateCar return null if GetCarRepository returns null

--- src/data/usecases/update-car/db-update-car.ts
+++ src/data/usecases/update-car/db-update-car.ts
@@ -1,14 +1,19 @@
+import { GetCarRepository } from '@/data/protocols/db/get-car-repository'
 import { UpdateCarRepository } from '@/data/protocols/db/update-car-repository'
 import { CarModel } from '@/domain/models/car'
 import { UpdateCar, UpdateCarModel } from '@/domain/usecases/car'
 
 export class DbUpdateCar implements UpdateCar {
   constructor(
-    private readonly updateCarRepository: UpdateCarRepository
+    private readonly updateCarRepository: UpdateCarRepository,
+    private readonly getCarRepository: GetCarRepository
   ) {}
 
   async update(carId: string, carData: UpdateCarModel):Promise<CarModel> {
-    await this.updateCarRepository.update(carId, carData)
-    return null
+    const isExists = await this.getCarRepository.get(carId)
+    if (!isExists) return null
+
+    const car = await this.updateCarRepository.update(carId, carData)
+    return car
   }
 }
