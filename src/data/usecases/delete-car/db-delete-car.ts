794483a391fdabeca841e8ddb9157ad09dabf64e
daniel otaviano <danielotavianopontes@gmail.com>
2021-07-15 01:11:03 -0300

    feat: ensure DbDeleteCar return null if DeleteCarRepository returns null

--- src/data/usecases/delete-car/db-delete-car.ts
+++ src/data/usecases/delete-car/db-delete-car.ts
@@ -1,10 +1,12 @@
 import { DeleteCarRepository } from '@/data/protocols/db/delete-car-repository'
+import { GetCarRepository } from '@/data/protocols/db/get-car-repository'
 import { CarModel } from '@/domain/models/car'
 import { DeleteCar } from '@/domain/usecases/car'
 
 export class DbDeleteCar implements DeleteCar {
   constructor(
-    private readonly deleteCarRepository: DeleteCarRepository
+    private readonly deleteCarRepository: DeleteCarRepository,
+    private readonly getCarRepository: GetCarRepository
   ) {}
 
   async delete(carId: string):Promise<CarModel> {
