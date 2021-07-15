b97a3910aca8f857ce7eb62844b5d5889491b449
daniel otaviano <danielotavianopontes@gmail.com>
2021-07-14 22:19:00 -0300

    fix: rename adapters with the correct name

--- src/main/routes/car-routes.ts
+++ src/main/routes/car-routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { adaptRoute } from '../adpters/express-route.adapter'
+import { adaptRoute } from '../adapters/express-route.adapter'
 import { makeAddCarController } from '../factories/controllers/car/add-car/add-car-controller-factory'
 
 export default (router: Router): void => {
